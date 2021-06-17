590ce4f032b34f6d17b1e5c40b6a42d7ef6bc381
SAMYAK JAIN <54874476+samyak1326@users.noreply.github.com>
2021-06-17 10:53:29 +0530

    Add files via upload

--- WordCountDriver.java
+++ WordCountDriver.java
@@ -1,3 +1,4 @@
+//package WrdCnt;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -10,32 +11,26 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-
-public class WordCountDriver 
+public class MyDriver 
 {
-	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException, URISyntaxException 
+	public static void main(String[] args) throws IOException,ClassNotFoundException, InterruptedException 
 	{
+
+
 		Configuration conf = new Configuration();
-		j.setJobName("My First Job");
-		j.setJarByClass(WordCountDriver.class );
-		j.setMapperClass(WordCountMapper.class );
-		j.setReducerClass(WordCountReducer.class);
+		Job job = new Job(conf, "Evenodd");  
+		
+		////name of Driver class
+		job.setJarByClass(MyDriver.class);
+		//name of mapper class
+		job.setMapperClass(MyMapper.class);
+		// name of reducer class
+		job.setReducerClass(MyReducer.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-		j.setOutputKeyClass(Text.class);
-		j.setOutputValueClass(IntWritable.class);
-		
-		FileInputFormat.addInputPath(j, new Path(args[0]));
-		FileOutputFormat.setOutputPath(j, new Path(args[1]));
-		
-		URI uri = new URI(args[1].toString());
-		
-		FileSystem fs =  FileSystem.get(uri, conf);
-		
-		boolean x = fs.delete(new Path(uri),true);
-		
-		
-		int y =  j.waitForCompletion(true) ? 0 : 1;
-		
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
-
 }
--- WordCountReducer.java
+++ WordCountReducer.java
@@ -1,24 +1,26 @@
 import java.io.IOException;
-import java.util.Iterator;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.Reducer.Context;
 
-public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable>
+public class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable> 
 {
-
-	@Override
-	public void reduce(Text key, Iterable<IntWritable> values, Context context)
-	throws IOException, InterruptedException 
+	
+	// reducer will recieve key value pair as  ODD [ 85 131 993 491 539 985 413.....
+    // 	EVEN [ 392 870 240 888 184 494 704 996 408....
+	
+	public void reduce(Text key, Iterable<IntWritable> values, Context context)	throws IOException, InterruptedException
 	{
-		int y = 0;
-		for(IntWritable x : values)
+		int sum = 0;
+		if(key.equals("PRIME"))
 		{
-			y ++;
-			
+			for (IntWritable value : values)
+			{
+				sum += value.get();
+			}
 		}
-		context.write(key, new IntWritable(y) );
+		context.write(key,new IntWritable(sum));;
 	}
 }
